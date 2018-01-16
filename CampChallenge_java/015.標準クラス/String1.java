d873bf4d1d75293d02aff813e81fe5067ff6299c
sitakagenki <sitakagenki0415@gmail.com>
2018-01-16 11:41:36 +0900

    JAVA課題【標準クラス】を送信致します。よろしくお願い致します。

---
+++ CampChallenge_java/015.標準クラス/String1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HyoujunnClass;
+
+/**
+ *
+ * @author sitak
+ */
+public class String1 {
+    public static void main(String[] args){
+    
+    String name="志鷹元気";
+    
+    System.out.println(name.length());
+    System.out.println(name.getBytes().length);
+    
+    }
+    
+}
